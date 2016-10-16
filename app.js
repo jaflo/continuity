4ec524812025b72327bb89fa57742e2dd7c66570
glooga <glooga@live.com>
2016-10-16 13:23:32 -0500

    changed express.router to app:

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -23,8 +20,7 @@ app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+var routes = require('./routes/index')(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
