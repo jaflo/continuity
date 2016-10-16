5094a146a9402607e25a48198a37cc7a382ee714
glooga <glooga@live.com>
2016-10-16 13:23:32 -0500

    Uh huh, this my shit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,4 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+module.exports = function(app) {
+    require('./main.js')(app);
+    require('./user.js')(app);
+};
