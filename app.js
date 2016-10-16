f099810428f3bfe178d5ee6da2f26e1ec14a2167
glooga <glooga@live.com>
2016-10-16 13:34:34 -0500

    restructured routes

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
 var app = express();
 
 // view engine setup
@@ -20,7 +23,8 @@ app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-var routes = require('./routes/index')(app);
+app.use('/', routes);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
