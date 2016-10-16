c0b1bb50e24a692e099477fdc424596586087878
SaltyQuetzals <quontas@gmail.com>
2016-10-16 14:50:27 -0500

    Switched main and index routes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
 module.exports = function(app, User, Story) {
-    require("./main.js")(app, User, Story);
-    require("./user.js")(app);
+	require("./user.js")(app);
+	require("./main.js")(app, User, Story);
 };
